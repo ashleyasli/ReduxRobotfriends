8317658a27cecc64ded74f903a628a1e69e518de
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2018-11-19 16:28:41 -0500

    adding actions,reducers,store, connect,provider

---
+++ src/containers/App.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList';
+/*import {robots} from './robots';*/
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+import {setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField		
+	}
+}
+
+const mapDispacthToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: []
+		}
+	}
+
+	componentDidMount() {
+		/*console.log(this.props.store.getState())*/
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then( response => response.json())
+		.then(users => this.setState({robots: users}));
+		
+	}
+	
+	render() {
+		const {robots} =this.state;
+		const { searchField, onSearchChange }=this.props;
+		const filteredRobots = robots.filter(robot => {
+		return  robot.name.toLowerCase().includes(searchField.toLowerCase());
+		/*console.log(filteredRobots);*/
+	})
+		if (!robots.length) {
+		return <h1>Loading</h1>
+	} else {
+		return(
+		<div className='tc'>
+			<h1 className='f1'>RobotFriends</h1>
+			<SearchBox searchChange={onSearchChange}/>
+			<Scroll>
+				<ErrorBoundary>
+					<CardList robots = {filteredRobots} />
+				</ErrorBoundary>
+			</Scroll>
+		</div>	
+			);
+	}
+	
+}
+}	
+
+export default connect(mapStateToProps, mapDispacthToProps)(App);
+
